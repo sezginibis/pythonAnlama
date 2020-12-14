11c74b0048f3e6e6200d6187e6c91d4d565c2f61
Sezgin İbiş (incelemeelemani) <34094261+sezginibis@users.noreply.github.com>
2020-12-14 22:06:40 +0300

    Biraz daha bilgi
    Noktalama işaretlerinin atıldığı ve kelimelerin sayıldığı çözümleri birleştirdim.

--- kelimeSiralamaca.py
+++ kelimeSiralamaca.py
@@ -14,8 +14,8 @@ print(kelimeler)
 
 for kelime in kelimeler:
     print(kelime) 
-
-print("--------------Şimdi de daha sade bir çözüm bulalım.-----------")
+# Fakat yukarıdaki her iki çözüm de, noktalama işaretlerinin yok edilerek sayılması çözümünü içermiyordu.
+print("--------------Şimdi de noktalama işaretleri için daha sade ve güzel bir çözüm bulalım. Ayrıca kelimeleri sayalım.-----------")
 cumle2 = "Bugün hava -çok |güzel. \$%()\{\}\[\]bana \" hava \' gerçekten.,!:;/ Evet güzel bir gün, gerçekten. Havada bulut yok ve işte bu yüzden güzel bir gün."
 
 for noktalama in '''-|\$%(){}[]"'.,!:;/''':
